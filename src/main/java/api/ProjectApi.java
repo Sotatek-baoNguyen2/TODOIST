86e9220c92dc30041c4aadcdf0996336d347d942
Bảo <bao.nguyen2@sotatek.com>
2021-04-12 13:57:22 +0700

    commit

---
+++ src/main/java/api/ProjectApi.java
@@ -0,0 +1,29 @@
+package API;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import MODEL.Config;
+import io.restassured.http.ContentType;
+
+public class ProjectApi {
+	public void createProject(String name) {
+		JSONObject request = new JSONObject();
+		request.put("name", name);
+
+		given().body(request.toJSONString()).header("Content-Type", "application/json")
+				.header("Authorization", "Bearer " + Config.token).when()
+				.post("https://api.todoist.com/rest/v1/projects").then().statusCode(200).log().all();
+	}
+	@Test
+	public void getAllProject() throws UnsupportedEncodingException {
+		given().header("Authorization", "Bearer " + Config.token).when()
+				.get("https://api.todoist.com/rest/v1/projects").then().contentType(ContentType.JSON).statusCode(200)
+				.log().all().extract().response();
+	}
+}
